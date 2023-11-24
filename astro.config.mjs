707c04a3fa018c8902121fe98ec5d079034a51f4
Garwood <lg829700@sunycnse.com>
2023-11-24 13:43:48 -0500

    adding rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://gorgeous-cupcake-5d1f0e.netlify.app/tags/"
+});
