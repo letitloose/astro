4735d94031680d9197d31427e393ad2b10e56a90
Garwood <lg829700@sunycnse.com>
2023-11-24 13:51:05 -0500

    fixing rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gorgeous-cupcake-5d1f0e.netlify.app/tags/"
+    site: "https://gorgeous-cupcake-5d1f0e.netlify.app/"
 });
