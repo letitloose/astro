6746274fbbd676f86bcaea152d402d71f9a87ada
Garwood <lg829700@sunycnse.com>
2023-11-27 09:50:57 -0500

    adding islands and interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gorgeous-cupcake-5d1f0e.netlify.app/"
+  site: "https://gorgeous-cupcake-5d1f0e.netlify.app/",
+  integrations: [preact()]
 });
